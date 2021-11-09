2a23c2c70b068ab7bd26e8e2b2e5a3ba517365c9
Kailash Chandra Bhakta <kcbhakta.99@gmail.com>
2021-11-09 14:02:31 +0530

    Image for form imported

---
+++ src/Parse.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Parse() {
+  return (
+    <div>
+        <Link to="/"><h1>App</h1></Link>
+    </div>
+  );
+}
+
+export default Parse;
